a81e0b8fb4b8e9132cd1c3934d9ea1daa8edf19c
tvme <ersan@tvme.ru>
2015-09-09 01:47:38 +0300

    function cut_file     read from source_file in dir_path num_lines from start_line     save results in target_file

--- preparing_data.py
+++ preparing_data.py
@@ -14,3 +14,27 @@ def unzip_gz(dir_path, file_name_gz, file_name_csv):
             file_csv.write(file_content)
     print 'File ' + file_name_gz + ' successfully unpacked'
     print 'File ' + file_name_csv + ' successfully written'
+
+def cut_file(dir_path, source_file, target_file, num_lines, start_line=0):
+    """
+    read from source_file in dir_path num_lines from start_line
+    save results in target_file
+    """
+    bufsize = 65536
+    with open(dir_path+target_file, 'w') as f_target:
+        with open(dir_path+source_file) as infile:
+            line_position = 0
+            while True:
+                lines = infile.readlines(bufsize)
+                if not lines or line_position > start_line + num_lines:
+                    break
+                for line in lines:
+                    if line_position >= start_line and line_position < start_line + num_lines:
+                        f_target.write(line)
+                    line_position += 1
+
+if __name__ == '__main__':
+    dir_path = 'C:\Users\User\PycharmProjects\ibcda\data\\'
+    source_file = 'source_test.csv'
+    target_file = 'test.csv'
+    cut_file(dir_path, source_file, target_file, 100, 777)
